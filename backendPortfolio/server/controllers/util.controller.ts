0331b8a029ae90d3ee6549d456e0150558e2b85a
Filipi <flf.2008brasil@hotmail.com>
2020-07-04 20:56:30 -0300

    changes

--- backendPortfolio/server/controllers/util.controller.ts
+++ backendPortfolio/server/controllers/util.controller.ts
@@ -25,33 +25,7 @@ export const EmaiController = () => {
       from: '"flf.2008brasil@hotmail.com"', // sender address
       to: 'flf.2008brasil@hotmail.com', // list of receivers
       subject: req.body.subject, // Subject line
-      text: req.body.text, // plain text bodyhtml: "<b>Hello world?</b>", // html body
-    };
-
-    transporter.sendMail(message, (error, info) => {
-      if (error) {
-        console.log(error);
-        res.status(400).end();
-      } else {
-        res.status(200).send('E-mail sent');
-      }
-    });
-  };
-
-  const sendEmailConfirmation = async (req: Request, res: Response) => {
-    const { email } = req.body;
-
-    const userId = await User.findOne({ where: { email } });
-
-    const token = jwt.sign(
-      { id: userId.id },
-      process.env.SECRET_TOKEN as string
-    );
-    const message = {
-      from: 'flf.2008brasil@hotmail.com', // sender address
-      to: 'flf.2008brasil@hotmail.com', // list of receivers
-      subject: 'Confirmação de conta', // Subject line
-      html: `<h1> Confirme a sua conta acessando o link  </h1> <a href='http://localhost:3000/confirmation/${token}'> Clique aqui...</a> `, // plain text bodyhtml: "<b>Hello world?</b>", // html body
+      html: req.body.text, // plain text bodyhtml: "<b>Hello world?</b>", // html body
     };
 
     transporter.sendMail(message, (error, info) => {
@@ -65,7 +39,6 @@ export const EmaiController = () => {
   };
 
   router.post('/', sendEmail);
-  router.post('/confirmation', sendEmailConfirmation);
 
   return router;
 };
